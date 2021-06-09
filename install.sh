f2e2ad6075ca0a4799b33ac4c40853cc4d05f590
Titouan CREACH <t.creach@unitee.io>
2021-06-09 09:41:07 +0000

    update

--- install.sh
+++ install.sh
@@ -5,14 +5,10 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-
 mkdir -p $HOME/.config/nvim
 ln -sf $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $PWD/zsh/zshrc $HOME/.zshrc
-
-git config --global alias.co 'checkout'
-git config --global alias.up 'pull --rebase --autostash'
-git config --global fetch.prune true
+ln -sf $PWD/git/gitconfig $HOME/.gitconfig
 
 
 echo "🎉 Installation completed 🎉"
