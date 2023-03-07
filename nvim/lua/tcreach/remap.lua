0b31e7b2ed6f20751c765efd67ec3f3276ebf6e7
Titouan CREACH <t.creach@unitee.io>
2023-03-07 09:50:45 +0100

    Bind some vscode commande to remap.lua

--- nvim/lua/tcreach/remap.lua
+++ nvim/lua/tcreach/remap.lua
@@ -7,10 +7,33 @@ vim.keymap.set("n", "<C-k>", "<C-W>k")
 
 vim.keymap.set("n", "//", ":nohlsearch<CR>")
 
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set("n", "<Up>", "<Nop>")
 vim.keymap.set("n", "<Down>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 vim.keymap.set("n", "<Right>", "<Nop>")
+
+vim.keymap.set("n", "-", "[[<Cmd>call VSCodeNotify('git.openFile')<CR>]]")
+vim.keymap.set("n", "+", "[[<Cmd>call VSCodeNotify('git.openChange')<CR>]]")
+
+vim.keymap.set(
+    'n',
+    '<leader>t',
+    [[<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>]]
+)
+
+vim.keymap.set(
+    'n',
+    '<leader>g',
+    [[<Cmd>call VSCodeNotify('workbench.view.scm')<CR>]]
+)
+
+
+vim.keymap.set(
+    'n',
+    'cc',
+    [[<Cmd>call VSCodeNotify('git.commitStaged')<CR>]]
+)
+
