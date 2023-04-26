cc40858f36dfe9794b4871984638105bf4467c15
Titouan CREACH <t.creach@unitee.io>
2023-04-26 17:20:49 +0200

    lunar vim

---
+++ lvim/config.lua
@@ -0,0 +1,244 @@
+--[[
+ THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+ `lvim` is the global options object
+]]
+-- vim options
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.relativenumber = false
+vim.opt.number = false
+
+vim.opt.listchars = { eol = '↵', space = '·', tab = '>~' }
+vim.opt.list = true
+
+-- general
+lvim.log.level = "info"
+lvim.format_on_save = {
+    enabled = true,
+    pattern = "*.lua",
+    timeout = 1000,
+}
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
+lvim.leader = ","
+-- add your own keymapping
+
+lvim.keys.normal_mode["<C-p>"] = ":Telescope git_files<CR>"
+lvim.keys.normal_mode["<C-b>"] = ":Telescope buffers<CR>"
+lvim.keys.normal_mode["<C-f>"] = ":Telescope live_grep<CR>"
+lvim.keys.normal_mode["<leader>a"] = ":lua vim.lsp.buf.code_action()<CR>"
+
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+-- -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
+-- -- Change theme settings
+lvim.colorscheme = "lunar"
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
+
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
+
+-- -- always installed on startup, useful for parsers without a strict filetype
+-- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
+
+-- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
+
+-- --- disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+lvim.lsp.on_attach_callback = function(client, bufnr)
+    if client.name == "omnisharp" then
+        client.server_capabilities.semanticTokensProvider = {
+            full = vim.empty_dict(),
+            legend = {
+                tokenModifiers = { "static_symbol" },
+                tokenTypes = {
+                    "comment",
+                    "excluded_code",
+                    "identifier",
+                    "keyword",
+                    "keyword_control",
+                    "number",
+                    "operator",
+                    "operator_overloaded",
+                    "preprocessor_keyword",
+                    "string",
+                    "whitespace",
+                    "text",
+                    "static_symbol",
+                    "preprocessor_text",
+                    "punctuation",
+                    "string_verbatim",
+                    "string_escape_character",
+                    "class_name",
+                    "delegate_name",
+                    "enum_name",
+                    "interface_name",
+                    "module_name",
+                    "struct_name",
+                    "type_parameter_name",
+                    "field_name",
+                    "enum_member_name",
+                    "constant_name",
+                    "local_name",
+                    "parameter_name",
+                    "method_name",
+                    "extension_method_name",
+                    "property_name",
+                    "event_name",
+                    "namespace_name",
+                    "label_name",
+                    "xml_doc_comment_attribute_name",
+                    "xml_doc_comment_attribute_quotes",
+                    "xml_doc_comment_attribute_value",
+                    "xml_doc_comment_cdata_section",
+                    "xml_doc_comment_comment",
+                    "xml_doc_comment_delimiter",
+                    "xml_doc_comment_entity_reference",
+                    "xml_doc_comment_name",
+                    "xml_doc_comment_processing_instruction",
+                    "xml_doc_comment_text",
+                    "xml_literal_attribute_name",
+                    "xml_literal_attribute_quotes",
+                    "xml_literal_attribute_value",
+                    "xml_literal_cdata_section",
+                    "xml_literal_comment",
+                    "xml_literal_delimiter",
+                    "xml_literal_embedded_expression",
+                    "xml_literal_entity_reference",
+                    "xml_literal_name",
+                    "xml_literal_processing_instruction",
+                    "xml_literal_text",
+                    "regex_comment",
+                    "regex_character_class",
+                    "regex_anchor",
+                    "regex_quantifier",
+                    "regex_grouping",
+                    "regex_alternation",
+                    "regex_text",
+                    "regex_self_escaped_character",
+                    "regex_other_escape",
+                },
+            },
+            range = true,
+        }
+    end
+end
+
+
+lvim.builtin.project.patterns = { ".git", ".csproj", "package.json" };
+
+-- -- linters, formatters and code actions <https://www.lunarvim.org/docs/languages#lintingformatting>
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "stylua" },
+--   {
+--     command = "prettier",
+--     extra_args = { "--print-width", "100" },
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     command = "shellcheck",
+--     args = { "--severity", "warning" },
+--   },
+-- }
+-- local code_actions = require "lvim.lsp.null-ls.code_actions"
+-- code_actions.setup {
+--   {
+--     exe = "eslint",
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+--
+
+lvim.plugins = {
+    {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
+    {
+        "rlane/pounce.nvim",
+        keys = {
+            { "s", "<cmd>Pounce<CR>" }
+        }
+    },
+    {
+        "github/copilot.vim"
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = {
+            "G",
+            "Git",
+            "Gdiffsplit",
+            "Gread",
+            "Gwrite",
+            "Ggrep",
+            "GMove",
+            "GDelete",
+            "GBrowse",
+            "GRemove",
+            "GRename",
+            "Glgrep",
+            "Gedit"
+        },
+        ft = { "fugitive" }
+    },
+    {
+        "tpope/vim-surround",
+    },
+    {
+        "chrisgrieser/nvim-various-textobjs",
+        config = function()
+            require("various-textobjs").setup({ useDefaultKeymaps = true })
+        end,
+    },
+}
+
+-- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
