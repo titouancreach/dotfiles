717c8ecb94d99a8f9435f5a911a9388dc320bf82
Titouan CREACH <t.creach@unitee.io>
2023-06-21 09:46:53 +0200

    Test neogit and diffview instead of lazygit

--- lvim/lua/tcreach/plugins.lua
+++ lvim/lua/tcreach/plugins.lua
@@ -211,5 +211,21 @@ lvim.plugins = {
     },
     {
         'nvim-treesitter/playground'
-    }
+    },
+    {
+        'TimUntersberger/neogit',
+        dependencies =
+        {
+            'nvim-lua/plenary.nvim',
+            'sindrets/diffview.nvim'
+        },
+        config = function()
+            require('neogit').setup({
+                integrations = {
+                    diffview = true
+                }
+            })
+        end
+    },
+    { "sindrets/diffview.nvim" }
 }
