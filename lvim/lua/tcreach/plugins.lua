a74f26b4df84d8e63e03cc488d136ad7a6eb8e0a
Titouan CREACH <t.creach@unitee.io>
2023-07-27 23:32:47 +0200

    style(neovim): unused comments

--- lvim/lua/tcreach/plugins.lua
+++ lvim/lua/tcreach/plugins.lua
@@ -157,9 +157,7 @@ lvim.plugins = {
 
         "folke/flash.nvim",
         event = "VeryLazy",
-        ---@type Flash.Config
         opts = {},
-        -- stylua: ignore
         keys = {
             { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
             {
